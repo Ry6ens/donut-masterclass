9299f76e5037276968496799346ec8f122709642
Rostyslav Pobidynskyi <pobedarostik@gmail.com>
2021-05-15 23:59:25 +0300

    Новий скрипт

--- src/readMore.js
+++ src/readMore.js
@@ -1,11 +1,12 @@
-function readMore() {
-    var more = document.getElementById("more");
-    var btn = document.getElementById("btn");
-    if (more.style.display === "inline") {
-        btn.innerHTML = "Read More";
-        more.style.display = "none"
-    } else {
-        btn.innerHTML = "Less";
-        more.style.display = "inline";
-    }
+let btn = document.querySelector('#btn');
+btn.onclick = function () {
+  let more = document.getElementById("more");
+  let btn = document.getElementById("btn");
+  if (more.style.display === "inline") {
+    btn.innerHTML = "Read More";
+    more.style.display = "none"
+  } else {
+    btn.innerHTML = "Less";
+    more.style.display = "inline";
+  }
 }

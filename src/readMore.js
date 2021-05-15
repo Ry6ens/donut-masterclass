bb63f86bc24db03d8dca5d041ba17eb5057bfba2
Rostyslav Pobidynskyi <pobedarostik@gmail.com>
2021-05-15 23:03:22 +0300

    Скрипт кнопки "Read More" через Id

--- src/readMore.js
+++ src/readMore.js
@@ -1,7 +1,6 @@
 function readMore() {
-    var more = document.getElementsById("more");
+    var more = document.getElementById("more");
     var btn = document.getElementById("btn");
-
     if (more.style.display === "inline") {
         btn.innerHTML = "Read More";
         more.style.display = "none"

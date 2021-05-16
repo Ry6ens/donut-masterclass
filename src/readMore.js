1dd9acb6260c010857fa8a4791f59c0bd11c482e
Rostyslav Pobidynskyi <pobedarostik@gmail.com>
2021-05-16 21:35:37 +0300

    заміна скрипта з домашки на новий

--- src/readMore.js
+++ src/readMore.js
@@ -1,24 +1,24 @@
-// let btn = document.querySelector('.about-us__button');
-// btn.onclick = function () {
-//   let more = document.querySelector(".more");
-//   let btn = document.querySelector(".about-us__button");
-//   if (more.style.display === "inline") {
-//     btn.innerHTML = "Read More";
-//     more.style.display = "none"
-//   } else {
-//     btn.innerHTML = "Less";
-//     more.style.display = "inline";
-//   }
-// }
+let btn = document.querySelector('.about-us__button');
+btn.onclick = function () {
+  let more = document.querySelector(".more");
+  let btn = document.querySelector(".about-us__button");
+  if (more.style.display === "inline") {
+    btn.innerHTML = "Read More";
+    more.style.display = "none"
+  } else {
+    btn.innerHTML = "Less";
+    more.style.display = "inline";
+  }
+}
 
-(() => {
-  const menuBtnRef = document.querySelector("[more-button]")
-  const mobileMenuRef = document.querySelector("[more-text]")
-  menuBtnRef.addEventListener("click", () => {
-    const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false
-    menuBtnRef.classList.toggle("is-open")
-    menuBtnRef.setAttribute("aria-expanded", !expanded)
+// (() => {
+//   const menuBtnRef = document.querySelector("[more-button]")
+//   const mobileMenuRef = document.querySelector("[more-text]")
+//   menuBtnRef.addEventListener("click", () => {
+//     const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false
+//     menuBtnRef.classList.toggle("is-open")
+//     menuBtnRef.setAttribute("aria-expanded", !expanded)
     
-    mobileMenuRef.classList.toggle("is-open")
-  })
-})()
+//     mobileMenuRef.classList.toggle("is-open")
+//   })
+// })()

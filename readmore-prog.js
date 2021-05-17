a92c64e67a94ef0b9ea87a94a6b1c53234e20aaa
Amphy-nick <d_burkovskui@ukr.net>
2021-05-17 12:46:40 +0300

    оновлений скріпт

--- readmore-prog.js
+++ readmore-prog.js
@@ -1,15 +1,3 @@
-// function readMore() {
-//     var more = document.getElementById("more");
-//     var btn = document.getElementById("btn");
-//     if (more.style.display === "inline") {
-//         btn.innerHTML = "Read More";
-//         more.style.display = "none"
-//     } else {
-//         btn.innerHTML = "Less";
-//         more.style.display = "inline";
-//     }
-// }
-
 (() => {
   const menuBtnRef = document.querySelector("[more-button-program]")
   const mobileMenuRef = document.querySelector("[more-text-program]")
@@ -17,7 +5,7 @@
     const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false
     menuBtnRef.classList.toggle("is-open")
     menuBtnRef.setAttribute("aria-expanded", !expanded)
-    
+
     mobileMenuRef.classList.toggle("is-open")
   })
 })()

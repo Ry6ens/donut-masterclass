ae69cc5ac500e210bc24bb127bfb780bfd4dc1ec
Nina Antoniuk <Antoniuk_Nina@ukr.net>
2021-05-23 18:53:49 +0300

    додала скрипт для закриття мобільного меню

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,10 +1,20 @@
 (() => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
-  const mobileMenuRef = document.querySelector("[data-menu]")
+  const mobileMenuRef = document.querySelector("[data-menu]");
+  const menuItem = document.querySelectorAll('.navigation-list__link');
+
+
   menuBtnRef.addEventListener("click", () => {
     const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false
     menuBtnRef.classList.toggle("is-open")
     menuBtnRef.setAttribute("aria-expanded", !expanded)
     mobileMenuRef.classList.toggle("is-open");
   });
+
+    for (i in menuItem) {
+    const item = menuItem[i];
+    item.addEventListener('click', () => mobileMenuRef.classList.remove('is-open'));
+  }
 })();
+
+
